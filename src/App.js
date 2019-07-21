b21ed70ddb211325f5e08dda709b1ae7feeb1a41
Duane McFarlane <duanemcfarlane@yahoo.com>
2019-07-21 19:41:10 -0400

    current react app

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 import UserInput from './UserInput/UserInput';
 
 class App extends Component {
-  state = {
-    currentFields: ['']
-  }
+  
 
-  clearFieldHandler = () => {
-    // console.log('was clicked')
-    // this.setState.UserInput = '';
+  clearFieldHandler = (event) => {
+  this.setState({
+
+  })
   }
 
   render() {
@@ -18,8 +17,10 @@ class App extends Component {
         <h1>Image Render App</h1>
         <h3>Input Width and Length Units to Generate a Preview</h3>
         <p>(Units are in inches)</p>
-        <UserInput value = "width" />
-        <UserInput value = "height" />
+        <form id="dimensions">
+          <UserInput />
+          <UserInput />
+        </form>
         <button>Submit</button>
         <button onClick={this.clearFieldHandler}>Clear</button>
       </div>
